cb5e48bff289fd7af31e2431bdd6f890feb05864
Ian Shepherd <ianshepherd77@gmail.com>
2019-10-12 17:06:41 +0100

    added account class and type files

---
+++ src/main/java/GitHubAccount.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class GitHubAccount {
+
+    private String userName;
+    private String accountName;
+    private HashMap<String, Repository> repositories;
+    private AccountType accountType;
+
+    public GitHubAccount(String userName, String accountName){
+        this.userName = userName;
+        this.accountName = accountName;
+        this.repositories = new HashMap<String, Repository>();
+        this.accountType = AccountType.FREE;
+    }
+
+
+}
